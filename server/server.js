60ba099d8f9bc972084fcb50fac07ee05ffd5e3b
vigneshxcode1 <vigneshxcode@gmail.com>
2025-10-30 22:35:13 +0530

    3 commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ console.log(process.env)
 app.use(cookieParser());
 app.use(
   cors({
-     origin: "http://localhost:5173",
+     origin: "https://zuntra-backend.onrender.com",
     credentials: true,  
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],             
